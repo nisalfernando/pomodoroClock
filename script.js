9a5b9af22b02fad3cc5b861918c5917ca8d4429a
Nisal Fernando <nisal.shiroma86@gmail.com>
2019-03-06 05:10:35 +0100

    Started JavaScript coding

---
+++ script.js
@@ -0,0 +1,6 @@
+/*****************/
+/*** Selectors ***/
+
+var modalBtn = document.getElementsByClassName("modal-btn");
+var modal = document.getElementsByClassName("modal-about");
+var cancel = document.getElementsByClassName("modal-cancel");
