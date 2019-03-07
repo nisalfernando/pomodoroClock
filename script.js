55d600e852aad39857bcf3202bbad8ced5a1f28a
Nisal Fernando <nisal.shiroma86@gmail.com>
2019-03-07 04:58:54 +0100

    Selected some elements

--- script.js
+++ script.js
@@ -4,3 +4,6 @@
 var modalBtn = document.getElementsByClassName("modal-btn");
 var modal = document.getElementsByClassName("modal-about");
 var cancel = document.getElementsByClassName("modal-cancel");
+
+var breakMins = document.getElementById("break-mins");
+var breakSecs = document.getElementById("break-secs");
