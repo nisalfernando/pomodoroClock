d2dd5f9927a5e763f33b6e2dfdd28abff31e17f9
Nisal Fernando <nisal.shiroma86@gmail.com>
2019-03-09 05:10:44 +0100

    Selecting the elements

--- script.js
+++ script.js
@@ -8,3 +8,5 @@ var cancel = document.getElementsByClassName("modal-cancel");
 var breakMins = document.getElementById("break-mins");
 var breakSecs = document.getElementById("break-secs");
 var mainMins = document.getElementById("main-mins");
+var mainSecs = document.getElementById("main-secs");
+var tally = document.querySelector('.tally-count');
