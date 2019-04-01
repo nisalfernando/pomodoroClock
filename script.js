ca4902cb2336444b1a5cbb01acf12b03373e90cd
Nisal Fernando <nisal.shiroma86@gmail.com>
2019-04-01 05:47:08 +0200

    Fixing bugs

--- script.js
+++ script.js
@@ -40,7 +40,7 @@ function timeRemaining(deadline) {
 
 function initClock() {
   const currentTime = new Date();
-  mainMinutes = mainMins.innerHTML;
+  mainMinutes = mainMins.innerText;
   mainDeadline = new Date(currentTime.getTime() + (mainMinutes * 60 * 1000));
   timeInterval = setInterval(function () {
     let x = timeRemaining(mainDeadline);
